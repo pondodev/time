f8d2e9e1690161ad0ae632111ee6b408344899d7
p0isonedpanda <daniel.coady@outlook.com.au>
2017-09-03 21:54:05 +1000

    Added simple menu controls

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Transform[] menuOptions;
+
+	int menuSelection;
+    
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        if (Input.GetKeyDown(KeyCode.W))
+            menuSelection = Mathf.Clamp(menuSelection - 1, 0, menuOptions.Length);
+		else if (Input.GetKeyDown(KeyCode.S))
+		    menuSelection = Mathf.Clamp(menuSelection + 1, 0, menuOptions.Length);
+
+		transform.position = Vector3.Lerp(transform.position, menuOptions[menuSelection].position, 0.1f);
+	}
+}
